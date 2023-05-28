ee97814624705ff149733515c1ec55ae86b953a5
Emman <emmanuel240158@gmail.com>
2023-05-28 03:05:36 -0500

    Stage 1/8 2+2

---
+++ src/main/kotlin/Stage 1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val numbers = readln().split(" ").map { it.toInt() }
+    val firstNumber = numbers[0]
+    val secondNumber = numbers[1]
+    println(sumNumbers(firstNumber,secondNumber))
+}
+
+fun sumNumbers(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
